8c8396f7c799534fd2d3b03e3b7be298f6b13532
_GodlyCow <godlycowcow@gmail.com>
2025-12-29 12:21:25 +0100

    fixed Eco api

--- api/src/main/java/com/serveressentials/api/PluginAPI.java
+++ api/src/main/java/com/serveressentials/api/PluginAPI.java
@@ -4,6 +4,7 @@ import com.serveressentials.api.afk.AFKAPI;
 import com.serveressentials.api.auction.AuctionAPI;
 import com.serveressentials.api.back.BackAPI;
 import com.serveressentials.api.daily.DailyAPI;
+import com.serveressentials.api.economy.EconomyAPI;
 import com.serveressentials.api.home.HomeAPI;
 import com.serveressentials.api.shop.ShopAPI;
 
@@ -17,4 +18,5 @@ public interface PluginAPI {
     AFKAPI getAFKAPI();
     BackAPI getBackAPI();
     DailyAPI getDailyAPI();
+    EconomyAPI getEconomyAPI();
 }
